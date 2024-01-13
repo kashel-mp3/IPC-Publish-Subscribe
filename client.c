cfccd16c2c5f4a23fb6e92085dcafb77c601e0b0
Anna Zalesińska <anna.zalesinska@protonmail.com>
2024-01-13 16:48:22 +0100

    implemented basic login validity check

--- client.c
+++ client.c
@@ -3,25 +3,72 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ID_LEN 3 //maximum id_len = 10
+#define USERNAME_LEN 10 // maximum username_len = 50
 
 struct login_msg {
     long mtype;
-    char id[20], username[20];
+    int id;
+    char username[USERNAME_LEN + 1];
 };
+
+int valid_id(char id_s[ID_LEN + 1]) {
+    int id = 0;
+    for(int i = 0; i < ID_LEN; i++) {
+        if(!isdigit(id_s[i])) {
+            printf("Invalid ID, try again\n %c \n", id_s[i]);
+            return 0;
+        }
+        id *= 10;
+        id += id_s[i] - '0';
+    }
+    printf("%d\n", id);
+    return id;
+}
 
 int send_login(int q_name) {
     struct login_msg login;
     login.mtype = 1;
-    printf("Provide ID:\n");
-    scanf("%s", (login.id));
+    char id_s[ID_LEN + 1];
+    bool done = 0;
+    int c;
+    while(!done) {
+        id_s[ID_LEN] = ' ';
+        printf("Provide ID (%d digits):\n", ID_LEN);
+        scanf("%4s", id_s);
+        while ((c = getchar()) != '\n');
+        if(strlen(id_s) < ID_LEN) {
+            printf("Invalid ID: too short, try again\n");
+            continue;
+        }
+        if(strlen(id_s) > ID_LEN) {
+            printf("Invalid ID: too long, try again\n");
+            continue;
+        }
+        login.id = valid_id(id_s);
+        if(!login.id) {
+            continue;
+        }
+        done = 1;
+    }
     printf("Provide username:\n");
-    scanf("%s", (login.username));
+    scanf("%20s", (login.username));
+    while ((c = getchar()) != '\n');
+    if(strlen(login.username) > USERNAME_LEN) {
+        printf("Invalid login: too long, try again\n");
+        return 1;
+    }
     msgsnd(q_name, &login, sizeof(struct login_msg) - sizeof(long), 0);
+    return login.id;
 }
 
 int main() {
     int server_q = 0;
-    send_login(server_q);
+    int own_q = send_login(server_q);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -2,18 +2,49 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <string.h>
 #include <sys/msg.h>
+
+#define MAX_CLIENTS 100
 
 struct message {
     long mtype;
-    char id[20], username[20];
+    int id;
+    char username[50];
 };
 
-int recieve_login(struct message* msg) {
-    printf("%s %s\n", msg->id, msg->username);
+struct Client {
+    int id;
+    char username[50];
+};
+
+int calculate_index(int clientID) {
+    return clientID % MAX_CLIENTS;
+}
+
+void print_client(struct Client* clients, int id) {
+    int index = calculate_index(id);
+    printf("ID: %d, Username: %s\n", clients[index].id, clients[index].username);
+}
+
+void print_clients(struct Client* clients) {
+    // Iterate through the user array and print each user
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i].id != 0) {  // Check if the slot is occupied
+            printf("ID: %d, Username: %s\n", clients[i].id, clients[i].username);
+        }
+    }
+}
+
+void add_client(struct Client* clients, int id, const char* username) {
+    int index = calculate_index(id);
+    clients[index].id = id;
+    strncpy(clients[index].username, username, sizeof(clients[index].username));
 }
 
 int main() {
+    struct Client clients[MAX_CLIENTS];
+
     int key = 111;
     int q_id = msgget(key, IPC_CREAT | 0666); 
     while(1) {
@@ -21,7 +52,8 @@ int main() {
         msgrcv(q_id, &msg, sizeof(struct message) - sizeof(long), 0, 0);
         switch (msg.mtype) {
         case 1:
-            recieve_login(&msg);
+            add_client(clients, msg.id, msg.username);
+            print_client(clients, msg.id);
             break;
         }
     }
