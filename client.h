abf1ff46b29a3255be7ce79af76f9b78c47f6afa
Anna Zalesińska <93271870+kashel-mp3@users.noreply.github.com>
2024-01-22 22:55:53 +0100

    Create client.h

---
+++ client.h
@@ -0,0 +1,11 @@
+int messageSend(int queueId, struct message* msg);
+struct message* messageReceive(int queueId, struct message* msg, long mtype);
+int send_login(int client_q , int server_q, const char* username);
+struct message subscribe_modify(int client_q , int server_q, const char* topicname, int duration);
+int unsubscribe(int client_q , int server_q, const char* topicname);
+int create_topic(int client_q , int server_q, const char* topicname);
+void display_topiclist(int client_q , int server_q, struct messageLogBuffer* mlb);
+int send_message(int server_q, int client_q , char* topic, char* username, char* text);
+struct message block_user(int server_q, int client_q, char* username);
+int checkTopic(int server_q, int client_q, char* topicname);
+char* parseInput(char* input, int* parsedInt);
